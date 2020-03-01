5c318ba71041a7bc94283a4b7ced50c1d51cf2d4
Daniel C. Bucher <daniel.bucher88@gmail.com>
2020-03-01 14:54:07 -0300

    Add baby-card-component icons and color property

--- baby-card-component/baby-card-component.js
+++ baby-card-component/baby-card-component.js
@@ -1,4 +1,7 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@polymer/iron-icon/iron-icon.js';
+import '@polymer/iron-icons/iron-icons.js';
+import '@polymer/iron-icons/communication-icons';
 
 /**
  * `baby-card-component`
@@ -19,27 +22,45 @@ class BabyCardComponent extends PolymerElement {
         .card {
           background-color: #fff;
           border-radius: 4px;
-          width: 163px;
-          height: 62px;
+          width: 150px;
+          height: 70px;
           box-shadow: 0px 3px 5px rgb(0,0,0, 0.4);
           font-family: Arial;
           font-size: 14px;
           font-weight: 600;
-          line-height: 16px;
-          color: #0F6DFF;
+          line-height: 19px;
           display: flex;
-          justify-content: center;
+          justify-content: space-between;
           align-items: center;
+          padding-left: 20px;
+          padding-right: 20px;
+        }
+
+        .icon {
+          --iron-icon-height: 40px;
+          --iron-icon-width: 40px;
+        }
+
+        .mr8 {
+          margin-right: 8px;
         }
       </style>
 
-      <div class="card">
-        <slot name="text"></slot>
+      <div class="card" style="color: [[color]];">
+        <iron-icon icon="book" class="icon mr8"></iron-icon>
+        <slot name="text" class="text"></slot>
+        <iron-icon icon="communication:call-made" class="icon"></iron-icon>
       </div>
     `;
   }
   static get properties() {
-    return {};
+    return {
+      color: {
+        type: String,
+        reflectToAttribute: true,
+        notify: true,
+      }
+    };
   }
 }
 
